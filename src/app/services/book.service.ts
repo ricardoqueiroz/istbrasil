95db7f78a08b12a7657793ad4d1ad24a972c2501
Ricardo Queiroz <Ricardo.queiroz@brmusic.com.br>
2026-01-15 14:32:29 -0300

    Correção no arquivo src/app/services/book.service.ts para que faça corretamente a chamada ao banco de dados.

--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 export interface Book {
   id: number;
@@ -23,7 +24,7 @@ export interface Book {
 })
 export class BookService {
   // Use a URL absoluta para garantir que bata no backend Node e não no servidor Angular
-  private apiUrl = 'http://localhost:3000/api/books'; 
+  private apiUrl = `${environment.apiUrl}/books`;
 
   constructor(private http: HttpClient) { }
 
